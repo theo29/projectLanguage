d0f24a0305a6e18546e771f56ff9f772464fe5eb
Jon <joaquim.jonathan@bluewin.ch>
2016-10-25 19:13:00 +0200

    25.10.16 Création de l'entité Choix exercices et Vocabulaire

---
+++ app/src/main/java/com/mycompany/projectlanguage/ChoixExercices.java
@@ -0,0 +1,13 @@
+package com.mycompany.projectlanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoixExercices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_exercices);
+    }
+}
