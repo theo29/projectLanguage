e1b3aabe2a9af31d9d708bb7416e664b85725605
Jon <joaquim.jonathan@bluewin.ch>
2016-10-25 10:42:55 +0200

    25.10.16 Création de l'entité ChoixLangues Création de l'entité ChoixDifficulte

---
+++ app/src/main/java/com/mycompany/projectlanguage/ChoixDifficulte.java
@@ -0,0 +1,13 @@
+package com.mycompany.projectlanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoixDifficulte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_difficulte);
+    }
+}
